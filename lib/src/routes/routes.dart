29efcfbf6c47336e921c92e5de4bb35e8b399c1c
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2021-08-30 16:26:53 -0400

    [MOD] Modified id of application

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:push_notifications/src/screens/home-screen.dart';
+import 'package:push_notifications/src/screens/message-screen.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     'home': (_) => HomeScreen(),
+    'message': (_) => MessageScreen(),
   };
 }
--- lib/src/screens/message-screen.dart
+++ lib/src/screens/message-screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,7 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         child: Center(
           child: Text(
-            'home screen',
+            'message screen',
             style: TextStyle(fontSize: 30, color: Colors.black38),
           ),
         ),
